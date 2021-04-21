6e78f6117ab53294f5e44d1598e8f178ce71d35e
mahmoudadel44 <mrmahmoudadel1996@gmail.com>
2021-04-21 22:45:52 +0200

    first commit

---
+++ style/js/offcanvas.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//   $(".fas").click(function () {
+//     $(".myUL").toggle("slow");
+//   });
+// });
+
+// $(".openNav").click(function () {
+//   $(".navdropdown").toggleClass("slidenavdown").animate(2000);
+// });
